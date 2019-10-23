04c9e204bf1579712dcca4e82377537ccdfa440d
theStaBita <JoLew04@gmail.com>
2019-10-23 13:59:02 -0400

    Modded app.method() call to myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-app.get("/",absolutePath = __dirname + /views/index.html/file.ext);
+app.get("/",absolutePath = __dirname + "/views/index.html");
 // --> 7)  Mount the Logger middleware here
 
 
