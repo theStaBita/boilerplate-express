f48d885fd3ae60f2c06625a19dbbd830a12343a3
John Lewis <JoLew04@gmail.com>
2019-10-25 01:12:08 -0400

    modified get response

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-process.env.MESSAGE_STYLE=uppercase;
+process.env.MESSAGE_STYLE="uppercase";
 if(process.env.MESSAGE_STYLE == uppercase) {app.get("/json", function(req, res) {res.json({"message": "HELLO JSON"});});}
 else{app.get("/json", function(req, res) {res.json({"message": "Hello json"});});}
 app.use(express.static(__dirname + "/public"))
