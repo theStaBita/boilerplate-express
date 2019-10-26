bdadf8dbf2a27f158085ada549a50b415d98dbd7
John Lewis <JoLew04@gmail.com>
2019-10-26 12:09:05 -0400

    modified get response

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ console.log("Hello World");
 
 /** 2) A first working Express Server */
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.send("Hello Express");
 })
 
 /** 3) Serve an HTML file */
